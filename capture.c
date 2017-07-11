fe4ac186eaea15c6f17daa6ddcee8e49903685b6
dima <alexeev6@yahoo.com>
2017-07-12 00:52:19 +0300

    Made right network byte order.

--- capture.c
+++ capture.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <netinet/in.h>
 #include <alsa/asoundlib.h>
 
+#define BUF_SIZE	8000
+
 struct au_header_s {
-	unsigned int magic;
-	unsigned int data_offset;
-	unsigned int data_size;
-	unsigned int encoding;
-	unsigned int sample_rate;
-	unsigned int channels;
+	uint32_t magic;
+	uint32_t hdr_size;
+	uint32_t data_size;
+	uint32_t encoding;
+	uint32_t sample_rate;
+	uint32_t channels;
 };
 
-struct au_header_s file_hdr = {
-    .magic = 0x2e736e64,
-    .data_offset = 24,
-    .data_size = 128*1000,
-    .encoding = 3,
-    .sample_rate = 44100,
-    .channels = 1
-};
 
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
     int i;
+    int ordi;
     int err;
-    short buf[128];
+    struct au_header_s au;
+    uint16_t buf[BUF_SIZE];
     snd_pcm_t *capture_handle;
-    snd_pcm_hw_params_t *hw_params;
 
     if ((err = snd_pcm_open (&capture_handle, argv[1],
                              SND_PCM_STREAM_CAPTURE, 0)) < 0) {
@@ -36,10 +33,10 @@ main (int argc, char *argv[])
         exit (1);
     }
     if ((err = snd_pcm_set_params(capture_handle,
-                                  SND_PCM_FORMAT_S16_LE,
+                                  SND_PCM_FORMAT_S16_BE,
                                   SND_PCM_ACCESS_RW_INTERLEAVED,
                                   1,
-                                  44100,
+                                  8000,
                                   0, //int  	soft_resample,
                                   0)) < 0) //unsigned int  	latency
     {
@@ -53,15 +50,24 @@ main (int argc, char *argv[])
         exit (1);
     }
 
+    au.magic = htonl(0x2e736e64);
+    au.hdr_size = htonl(18);
+    au.data_size = htonl(BUF_SIZE*1000);
+    au.encoding = htonl(3);
+    au.sample_rate = htonl(8000);
+    au.channels = htonl(1);
+
     puts("Read Now!");
-    write(fileno(stdout), &file_hdr, sizeof(file_hdr));
+    write(fileno(stdout), &au, sizeof(au));
     for (i = 0; i < 1000; ++i) {
-        if ((err = snd_pcm_readi(capture_handle, buf, 128)) != 128) {
+        if ((err = snd_pcm_readi(capture_handle, buf, BUF_SIZE)) != BUF_SIZE) {
             fprintf (stderr, "read from audio interface failed (%s)\n",
                      snd_strerror (err));
             exit (1);
         } else {
-	    write(fileno(stdout), buf, 128);
+	  //  for (ordi = 0; ordi < BUF_SIZE; ordi++)
+          //      buf[ordi] = htons(buf[ordi]); 
+	    write(fileno(stdout), buf, BUF_SIZE);
 	}
     }
 
